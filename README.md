088a76b53cd53dfc8f7b089f0db161767452cf66
Grace Choi <choi.g.330@gmail.com>
2020-12-17 20:49:58 -0800

    lab complete

---
+++ README.md
@@ -0,0 +1,26 @@
+# Garage Band with OOP
+
+**Author**: Grace Choi
+**Version**: 1.0.0 
+
+## Overview
+Creating a Garage Band with Object Oriented Programming.
+
+## Getting Started
+1. Use `Poetry` to create your project with name `pythonic-garage-band`
+2. Create a new repository named pythonic-garage-band.
+3. Use feature branches for your work (branch: "lab4")
+
+## Architecture
+Package Manager: `poetry`
+Modules: `pytest`, `yaml`, `json`
+
+## Feature Tasks
+Use Python classes to model a Band made up of different kinds of musicians.
+Start with Guitarist, Bassist, and Drummer.
+Make use of a Musician base class to handle common functionality which particular kinds of musicians will inherit.
+Make sure all provided unit tests pass (Band tests and Musician Subclass tests).
+
+## Change Log
+12.16.2020 - Lab Prep
+12.17.2020 - Lab
--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,23 +1,73 @@
 class Band:
-    def __init__(self, name, members=None):
+    """Band instance requires attribute of name (string) and members (list). The play_solos method asks each member to play a solo, in the order they were added to the band. The to_list method returns a list of previously created Band instances."""
+    instances = []
+    def __init__(self, name: str, members=None):
         self.name = name
         self.members = members
+        Band.instances.append(self)
 
     def __str__(self):
         return f"The band {self.name}"
 
     def __repr__(self):
         return f"Band instance. name={self.name}, members={self.members}"
+    
+    def play_solos(self):
+        solos = []
+        members = self.members
+        for solo in members:
+            solo = solo.play_solo()
+            solos.append(solo)
+        return solos
 
+        # one-liner solution from code review
+        # return [solo.play_solo() for solo in self.members]
+
+    @classmethod
+    def to_list(cls):
+        return cls.instances
 
 class Musician:
-    pass
+    """get_instrument method returns a string. play_solo method also returns a string."""
+    def __init__(self, name, instrument, solo, title):
+        self.name = name
+        self.instrument = instrument
+        self.solo = solo
+        self.title = title
 
-class Guitarist:
-    pass
+    def __str__(self):
+        return f"My name is {self.name} and I play {self.instrument}"
 
-class Bassist:
-    pass
+    def __repr__(self):
+        return f'{self.title} instance. Name = {self.name}'
+        # returns name of that class and refer to its name
+        # return f'{self.__class__.__name__} instance. Name = {self.name}'
 
-class Drummer:
-    pass
+    def get_instrument(self):
+            return f"{self.instrument}"
+
+    def play_solo(self):
+        return f"{self.solo}"
+
+class Guitarist(Musician):
+    def __init__(self, name):
+        super().__init__(name, "guitar", "face melting guitar solo", "Guitarist")
+
+class Bassist(Musician):
+    def __init__(self, name):
+        super().__init__(name, "bass", "bom bom buh bom", "Bassist")
+
+class Drummer(Musician):
+    def __init__(self, name):
+        super().__init__(name, "drums", "rattle boom crash", "Drummer")
+
+
+if __name__ == "__main__":
+    musician = Musician("joe", "flute", "solo", "title")
+    print(musician)
+    # print(repr(musician))
+
+    guitarist = Guitarist("jane")
+    print(guitarist)
+
+# Attribution - Super: https://realpython.com/python-super/
